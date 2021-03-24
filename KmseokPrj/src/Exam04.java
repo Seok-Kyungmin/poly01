f698ff5a9830fd185d798fb4daa113ea6af1cc65
Seok-Kyungmin <sukm386@naver.com>
2021-03-24 14:14:46 +0900

    오늘 두 번째 프로그래밍

---
+++ KmseokPrj/src/Exam04.java
@@ -0,0 +1,13 @@
+
+public class Exam04 {
+	public static void main(String[] args) {
+		boolean boo1, boo2;
+		
+		boo1 = true;
+		System.out.printf("%s, \n", boo1);
+		
+		boo2 = (10 == 20);
+		System.out.printf("%s \n", boo2);
+	}
+
+}
