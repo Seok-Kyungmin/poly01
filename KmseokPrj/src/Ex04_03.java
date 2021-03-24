3db1c0a4b4896791226f8ddb7eb8f038adaa33c2
Seok-Kyungmin <sukm38621@gmail.com>
2021-03-24 13:55:49 +0900

    오늘 첫 프로그래밍

---
+++ KmseokPrj/src/Ex04_03.java
@@ -0,0 +1,35 @@
+
+public class Ex04_03 {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		
+		 a++;
+		 System.out.println("a : " + a);
+		 System.out.println("a++ : " + a++);
+		 
+		 System.out.println("a : " + a);
+		 System.out.println("++a : " + ++a);
+		 
+		 a--;
+		 System.out.println("a : " + a);
+		 
+		 a+= 5;
+		 System.out.println("a : " + a);
+		 
+		 a-= 5;
+		 
+		 System.out.println("a : " + a);
+		 
+		 a *= 5;
+		 System.out.println("a : " + a);
+		 
+		 a /= 5;
+		 System.out.println("a : " + a);
+		 
+		 a %= 5;
+		 System.out.println("a : " + a);
+	}
+
+}
---
+++ KmseokPrj/src/Ex05_01.java
@@ -0,0 +1,19 @@
+
+public class Ex05_01 {
+
+	public static void main(String[] args) {
+		
+		int a = 100;
+		
+		if (a < 100) 
+			System.out.println("100보다 작군요..");
+			System.out.println("100보다 작군요22222");
+		
+		
+		
+		System.out.println("난 무조건 실행된다.");
+	} //100보다 작군요22222
+	  //난 무조건 실행된다.	
+	  //if문에 {}로 범위를 정하지 않으면 첫줄만 조건이 적용된다. 
+
+}
